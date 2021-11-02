f43dce3707014b7b909fe09cb468c50b794001b5
Gouldilocks <cagould@smu.edu>
2021-11-02 17:20:49 -0500

    final push for workflows

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,7 @@ const pool = require('./db')
 module.exports = function routes(app, logger) {
   // GET /
   app.get('/', (req, res) => {
-    res.status(200).send('This is the port for the API. Go to port 3000 to view the web page. Hello Alex');
+    res.status(200).send('This is the port for the API. Go to port 3000 to view the web page.');
   });
 
   // POST /reset
