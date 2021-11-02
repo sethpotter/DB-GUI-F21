704cb6e7d14c6197bb7ab839fa4fe702f16a46e7
Gouldilocks <cagould@smu.edu>
2021-11-02 16:02:41 -0500

    remove testing statment

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,7 @@ const pool = require('./db')
 module.exports = function routes(app, logger) {
   // GET /
   app.get('/', (req, res) => {
-    res.status(200).send('Go to 0.0.0.0:3000. Testing gh actions capability');
+    res.status(200).send('Go to 0.0.0.0:3000.');
   });
 
   // POST /reset
