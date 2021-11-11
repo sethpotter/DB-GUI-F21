455334b258e5c33c81504f9d7783341edd3011c8
Gouldilocks <cagould@smu.edu>
2021-11-11 16:39:51 -0600

    updated order routes and product routes

--- backend/routes.js
+++ backend/routes.js
@@ -6,96 +6,6 @@ module.exports = function routes(app, logger) {
     res.status(200).send('This is the port for the API. Go to port 3000 to view the web page.');
   });
 
-  // POST /reset
-  app.post('/reset', (req, res) => {
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if (err){
-        console.log(connection);
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection', err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query
-        connection.query('drop table if exists test_table', function (err, rows, fields) {
-          if (err) { 
-            // if there is an error with the query, release the connection instance and log the error
-            connection.release()
-            logger.error("Problem dropping the table test_table: ", err); 
-            res.status(400).send('Problem dropping the table'); 
-          } else {
-            // if there is no error with the query, execute the next query and do not release the connection yet
-            connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-              if (err) { 
-                // if there is an error with the query, release the connection instance and log the error
-                connection.release()
-                logger.error("Problem creating the table test_table: ", err);
-                res.status(400).send('Problem creating the table'); 
-              } else { 
-                // if there is no error with the query, release the connection instance
-                connection.release()
-                res.status(200).send('created the table'); 
-              }
-            });
-          }
-        });
-      }
-    });
-  });
-
-  // POST /multplynumber
-  app.post('/multplynumber', (req, res) => {
-    console.log(req.body.product);
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query and release connection
-        connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-          connection.release();
-          if (err) {
-            // if there is an error with the query, log the error
-            logger.error("Problem inserting into test table: \n", err);
-            res.status(400).send('Problem inserting into table'); 
-          } else {
-            res.status(200).send(`added ${req.body.product} to the table!`);
-          }
-        });
-      }
-    });
-  });
-
-  // GET /checkdb
-  app.get('/values', (req, res) => {
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query and release connection
-        connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-          connection.release();
-          if (err) {
-            logger.error("Error while fetching values: \n", err);
-            res.status(400).json({
-              "data": [],
-              "error": "Error obtaining values"
-            })
-          } else {
-            res.status(200).json({
-              "data": rows
-            });
-          }
-        });
-      }
-    });
-  });
-
   //  --------------------- productTable ---------------------------------
   // /productTable/{productID}
   // GET
@@ -112,7 +22,7 @@ module.exports = function routes(app, logger) {
 
         // if there is no issue obtaining a connection, execute query and release connection
         // Will need to change the query to the appropriately named table
-        connection.query('SELECT * FROM `db`.`product_table` WHERE productId = (?)', productId, function (err, rows, fields) {
+        connection.query('SELECT * FROM ProductTable WHERE productId = (?)', productId, function (err, rows, fields) {
           connection.release();
           if (err) {
             logger.error("Error while fetching values: \n", err);
@@ -146,7 +56,7 @@ module.exports = function routes(app, logger) {
         var image = req.body.image;
         var minVal = req.body.minVal;
 
-        var query = 'UPDATE `db`.`product_table`' +
+        var query = 'UPDATE ProductTable' +
                     'SET name = (?) ' 
                     'description = (?) ' +
                     'image = (?) ' + 
@@ -188,7 +98,7 @@ module.exports = function routes(app, logger) {
 
         // if there is no issue obtaining a connection, execute query and release connection
         // Will need to change the query to the appropriately named table
-        connection.query('DELETE * FROM `db`.`product_table` WHERE productId = (?)', productId, function (err, rows, fields) {
+        connection.query('DELETE * FROM ProductTable WHERE productId = (?)', productId, function (err, rows, fields) {
           connection.release();
           if (err) {
             logger.error("Error while fetching values: \n", err);
@@ -205,6 +115,37 @@ module.exports = function routes(app, logger) {
     });
   });
 
+  // /productTable/{orderID}
+  // GET
+  app.get('/productTable', (req, res) => {
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+        // Getting the Order Id from the URL parameters
+        var orderId = req.params('orderId');
+
+        // if there is no issue obtaining a connection, execute query and release connection
+        // Will need to change the query to the appropriately named table
+        connection.query('SELECT * FROM ProductTable WHERE orderId = (?)', orderId, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": JSON.stringify(rows)});
+          }
+        });
+      }
+    });
+  });
 
     //  --------------------- Order ---------------------------------
   // /Order
@@ -220,7 +161,7 @@ module.exports = function routes(app, logger) {
 
         // if there is no issue obtaining a connection, execute query and release connection
         // Will need to change the query to the appropriately named table
-        connection.query('SELECT * FROM `db`.`order_table` ', function (err, rows, fields) {
+        connection.query('SELECT * FROM `Order`', function (err, rows, fields) {
           connection.release();
           if (err) {
             logger.error("Error while fetching values: \n", err);
@@ -256,7 +197,7 @@ module.exports = function routes(app, logger) {
         var estArrival = req.body.estArrival;
         var delivered = req.body.delivered;
 
-        var query = 'INSERT INTO `db`.`order_table`(orderDate, deliveryAddress, carrier, sentDate, estArrival, delivered)' + 
+        var query = 'INSERT INTO `Order`(orderDate, deliveryAddress, carrier, sentDate, estArrival, delivered)' + 
                     'VALUES ((?)(?)(?)(?)(?)(?))';
 
         // if there is no issue obtaining a connection, execute query and release connection
@@ -278,37 +219,6 @@ module.exports = function routes(app, logger) {
     });
   });
 
-  // /order/{orderID}
-  // GET
-  app.get('/productTable', (req, res) => {
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // Getting the Order Id from the URL parameters
-        var orderId = req.params('orderId');
-
-        // if there is no issue obtaining a connection, execute query and release connection
-        // Will need to change the query to the appropriately named table
-        connection.query('SELECT * FROM `db`.`order_table` WHERE orderId = (?)', orderId, function (err, rows, fields) {
-          connection.release();
-          if (err) {
-            logger.error("Error while fetching values: \n", err);
-            res.status(400).json({
-              "data": [],
-              "error": "Error obtaining values"
-            })
-          } else {
-            res.status(200).json({
-              "data": JSON.stringify(rows)});
-          }
-        });
-      }
-    });
-  });
   
   // /order/{orderId}
   // PUT
