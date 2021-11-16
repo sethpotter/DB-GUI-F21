3984132823dcc3edbd7934549b413eed1e3cb529
phongsun <60372665+phongsun@users.noreply.github.com>
2021-11-16 16:24:02 -0600

    Almost done but state in main component resets for some reason

--- frontend/src/Pages/Shipment.jsx
+++ frontend/src/Pages/Shipment.jsx
@@ -1,26 +1,63 @@
 import React from "react";
+import "./styles.css";
 
-export const NewData = (props) => {
-  return (
-    <>
-      <h1>New Cart Item</h1>
-      <label htmlFor="item-select">Select Item to add. </label>
-      <br />
-      <select id="item-select" name="item-select"></select>
-      <br />
-      <label htmlFor="item-select">Select quantity. </label>
-      <br />
-      <input type="text" id="item-select" name="item-select" />
-      <br />
-    </>
-  );
-};
+export class NewData extends React.Component {
+  state = {
+    availableItems: [], // put available items here
+    item: "",
+    quantity: ""
+  };
+  saveItem = (event) => {
+    this.setState({ item: event.target.value });
+  };
+  saveQuantity = (event) => {
+    this.setState({ quantity: event.target.value });
+  };
+  toProp = (event) => {
+    alert("Selected: " + this.state.item + " " + this.state.quantity);
+    this.props.addNew(this.state.item, this.state.quantity);
+  };
+  test = (event) => {
+    alert("Click worked");
+  };
+
+  render() {
+    return (
+      <>
+        <h1>New Cart Item</h1>
+        <form onSubmit={this.test}>
+          <label htmlFor="item-select">Select Item to add. </label>
+          <br />
+          <select id="item-select" name="item-select" onChange={this.saveItem}>
+            <option></option>
+            <option value="TR-808">TR-808</option>
+            <option value="TB-303">TB-303</option>
+            <option value="SH-101">TB-303</option>
+          </select>
+          <br />
+          <label htmlFor="item-select">Select quantity. </label>
+          <br />
+          <input
+            type="text"
+            id="item-select"
+            onChange={this.saveQuantity}
+            name="item-select"
+          />
+          <br />
+          <input type="submit" value="Submit" class="submit-button" />
+          <button onClick={this.toProp}>Test</button>
+        </form>
+      </>
+    );
+  }
+}
 
 export class FormComponent extends React.Component {
   state = {
     result: []
   };
   render() {
+    //alert("Length: " + this.props.productName.length);
     for (let index in this.props.productName) {
       if (index < this.state.result.length) {
         continue;
@@ -33,6 +70,13 @@ export class FormComponent extends React.Component {
             </td>
           </tr>
         );
+        this.setState(this.state.result);
+        alert(
+          "Product name: " +
+            this.props.productName[index] +
+            " Quantity: " +
+            this.props.quantity[index]
+        );
       }
     }
     return <>{this.state.result}</>;
@@ -42,17 +86,41 @@ export class FormComponent extends React.Component {
 export class Form extends React.Component {
   state = {
     deliveryTo: "",
-    submitted: false
+    submitted: false,
+    items: [],
+    quantities: []
   };
 
   newItem = () => {
     this.state.submitted = 1;
     this.setState({ submitted: 1 });
   };
+  newItemAdded = (itemName, quantity) => {
+    alert("State set?");
+    this.state.items.push(itemName);
+    this.state.quantities.push(quantity);
+    this.setState(this.state.items);
+    alert("State set.");
+    this.setState(this.state.quantities);
+    alert("State has been set!");
+  };
   render() {
     let tag = "";
     if (this.state.submitted == true) {
-      tag = <NewData ok={this.state.submitted} />;
+      tag = (
+        <NewData
+          ok={this.state.submitted}
+          addNew={(itemName, quantity) => {
+            alert("State set?");
+            this.state.items.push(itemName);
+            this.state.quantities.push(quantity);
+            this.setState(this.state.items);
+            alert("State set.");
+            this.setState(this.state.quantities);
+            alert("State has been set!");
+          }}
+        />
+      );
     }
     return (
       <>
@@ -67,7 +135,10 @@ export class Form extends React.Component {
               <th>Product</th>
               <th>Quantity</th>
             </tr>
-            <FormComponent productName={[]} quantity={[]} />
+            <FormComponent
+              productName={this.state.items}
+              quantity={this.state.quantities}
+            />
           </table>
           <button
             type="button"
