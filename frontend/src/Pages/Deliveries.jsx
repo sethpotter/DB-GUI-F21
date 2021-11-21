a6b4dfe647d52e6c29dc2c55679334b36735f30d
phongsun <60372665+phongsun@users.noreply.github.com>
2021-11-20 21:43:40 -0600

    can now change the status

--- frontend/src/Pages/Deliveries.jsx
+++ frontend/src/Pages/Deliveries.jsx
@@ -1,95 +1,114 @@
 import React from "react";
 
 export const ChangeStatus = (props) => { // Select from 3 statuses with select menu
-  return(
-    <form>
-      <select>
-        <option value="scheduled">Scheduled</option>
-        <option value="inProgress">In Progress</option>
-        <option value="delivered">Delivered</option>
-      </select>
-    </form>
-  );
-}
-export class DeliveryComponent extends React.Component{
-  //TO-DO: find way to expand/contract based on index
-  //Have option to change status (IN PROGRESS)
-  //Once status changes notify the supplier
-  //Integrate adding shipments
-  state ={
-    button: "▼",
-    rowHeight: "1em",
-    status: <>{this.props.status}</>
-  };
-
-  editStatus = () => {
-    this.setState({status : <ChangeStatus 
-    id={this.props.id} 
-    index={this.props.index} 
-    status={this.props.status}
-    />});
-  }
-  switchArrow = () => {
-    if(this.state.button == "▼"){
-      this.setState({button: "▲"});
-    }else{
-      this.setState({button: "▼"});
-    }
-  }
-  render() {
-    let status = <></>;
-    return(
-      <tr>
-          <td>{this.props.id}</td>
-          <td>{this.props.item} <button type="button" className="expand" onCLick={this.switchArrow}>{this.state.button}</button></td>
-          <td>{this.state.status} <button type="button" onClick={this.editStatus}>Edit Status</button></td>
-      </tr>
+    return (
+        <form>
+            <select onChange={props.changeStatus}>
+                <option value="Scheduled">Scheduled</option>
+                <option value="In Progress">In Progress</option>
+                <option value="Delivered">Delivered</option>
+            </select>
+        </form>
     );
-  }
+}
+export class DeliveryComponent extends React.Component {
+    //TO-DO: find way to expand/contract based on index
+    //Once status changes notify the supplier
+    //Integrate adding shipments
+    state = {
+        button: "▼",
+        rowHeight: "1em",
+        status: <>{this.props.status}</>,
+        proxyStatus: '',
+        statusStatus: "Edit Status"
+    };
+
+    editStatus = () => {
+        if(this.state.statusStatus == "Edit Status"){
+            this.setState({
+                status: <ChangeStatus
+                    id={this.props.id}
+                    index={this.props.index}
+                    status={this.props.status}
+                    changeStatus={(event) => {
+                        this.props.changeStatus(event.target.value, this.props.index);
+                        this.setState({proxyStatus : event.target.value});
+                    }}
+                />,
+                statusStatus: "Done"
+            });
+        }else{
+            this.setState({
+                status: <>{this.state.proxyStatus}</>,
+                statusStatus: "Edit Status"
+            });
+        }
+    }
+    switchArrow = () => {
+        if (this.state.button == "▼") {
+            this.setState({ button: "▲" });
+        } else {
+            this.setState({ button: "▼" });
+        }
+    }
+    render() {
+        let status = <></>;
+        return (
+            <tr>
+                <td>{this.props.id}</td>
+                <td>{this.props.item} <button type="button" className="expand" onCLick={this.switchArrow}>{this.state.button}</button></td>
+                <td>{this.state.status} <button type="button" onClick={this.editStatus}>{this.state.statusStatus}</button></td>
+            </tr>
+        );
+    }
 }
 export class Deliveries extends React.Component {
-  // TO DO:
-  // Statuses set to 3 options
-  state = {
-    // shipment stuff, all the same length
-    ids : [3809080, 1083159],
-    items: ["Blue Dream, Lucky Charms, Strawberry Sugar", "D8 Bulk"],
-    statuses: ["Gucci", "Also Gucci"],
-    // rows to be printed
-    rows: []
-  };
-
-  // This is a separate thing, the Deliveries thing
-  render() {
-    let displayShipments = <></>;
-    if(this.state.ids.length == 0){
-      displayShipments=<h1>Add some shipments first!</h1>
-    }else{
-      for(let index in this.state.ids){
-        this.state.rows.push(<DeliveryComponent id={this.state.ids[index]} 
-        item ={this.state.items[index]}
-        status={this.state.statuses[index]}
-        index={index}
-        />)
-      }
-      displayShipments = 
-      <table>
-          <tr>
-            <th>Shipment ID</th>
-            <th>Items</th>
-            <th>Status</th>
-          </tr>
-          {this.state.rows}
-        </table>;
+    // TO DO:
+    // Statuses set to 3 options
+    state = {
+        // shipment stuff, all the same length
+        ids: [3809080, 1083159],
+        items: ["Blue Dream, Lucky Charms, Strawberry Sugar", "D8 Bulk"],
+        statuses: ["Gucci", "Also Gucci"],
+        // rows to be printed
+        rows: []
+    };
+    render() {
+        let displayShipments = <></>;
+        if (this.state.ids.length == 0) {
+            displayShipments = <h1>Add some shipments first!</h1>
+        } else {
+            for (let index in this.state.ids) {
+                if (index == this.state.rows.length) {
+                    this.state.rows.push(<DeliveryComponent id={this.state.ids[index]}
+                        item={this.state.items[index]}
+                        status={this.state.statuses[index]}
+                        index={index}
+                        changeStatus={(status, index) => {
+                            this.state.statuses[index] = status;
+                            this.setState(this.state.statuses);
+                        }}
+                    />)
+                }
+            }
+            displayShipments =
+                <table>
+                    <tr>
+                        <th>Shipment ID</th>
+                        <th>Items</th>
+                        <th>Status</th>
+                    </tr>
+                    {this.state.rows}
+                </table>;
+        }
+        //this.getShipments();
+        return (
+            <>
+                <h1>Deliveries</h1>
+                <p>Track your deliveries.</p>
+                {displayShipments}
+                {this.state.changeStatus}
+            </>
+        );
     }
-    //this.getShipments();
-    return (
-      <>
-        <h1>Deliveries</h1>
-        <p>Track your deliveries.</p>
-        {displayShipments}
-        {this.state.changeStatus}
-      </>
-    );
-  }
 }
