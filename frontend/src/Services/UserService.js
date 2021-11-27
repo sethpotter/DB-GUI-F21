9b9426ba432d7ad72e409b72db8bc3b603f8fbe3
SMUPotter <sapotter@smu.edu>
2021-11-26 23:16:40 -0600

    Updates to routes and correcting some stuff

---
+++ frontend/src/Services/UserService.js
@@ -0,0 +1,18 @@
+import {isLogin} from "../Util/utils";
+import {getLoggedIn} from "../Api/UserRoutes";
+
+export class UserService {
+
+    loadUser(callback) {
+        if(window.user)
+            callback(window.user);
+
+        if(isLogin()) {
+            getLoggedIn().then((user) => {
+                window.user = user;
+                callback(user);
+            });
+        }
+    }
+
+}
