31d930cde2ae73427f457c63b438d1f5cb582801
SMUPotter <sapotter@smu.edu>
2021-11-28 16:59:14 -0600

    Dashboard search working and add style

--- frontend/src/Components/ProductSearch.jsx
+++ frontend/src/Components/ProductSearch.jsx
@@ -3,16 +3,10 @@ import { } from '@material-ui/core';
 
 export const ProductSearch = props => {
     const [ name, setName ] = useState("");
-    const [ isFilter, setFilter ] = useState(false);
 
-    let filter = (!isFilter) ? 
-        <div>
-            <a className="d-flex justify-content-center" onClick={ () => setFilter(!isFilter) }>Filter Options</a>
-        </div>
-        : 
-        <div className="row">
-            <div className="col-4"></div>
-            <div className="col-2">
+    let filter =
+        <div className="d-flex flex-row">
+            <div className="px-3">
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="sort" id="default" value="default" defaultChecked/>
                     <label class="form-check-label" for="flexRadioDefault1">
@@ -32,52 +26,55 @@ export const ProductSearch = props => {
                     </label>
                 </div>
             </div>
-            <div className="col-2">
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault1" defaultChecked/>
-                <label class="form-check-label" for="flexRadioDefault1">
-                    All
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault2"/>
-                <label class="form-check-label" for="flexRadioDefault2">
-                    In Stock
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault3"/>
-                <label class="form-check-label" for="flexRadioDefault2">
-                    Low Stock
-                </label>
+            <div className="px-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault1" defaultChecked/>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        All
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault2"/>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        In Stock
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="filter" id="flexRadioDefault3"/>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        Low Stock
+                    </label>
+                </div>
             </div>
         </div>
-    </div>
         
 
     
 
-    return <div className="row">
-        <div className="col-1"></div>
-        <div className="col-8">
-            <div className="form-group">
-                <label htmlFor="search_name">Search Product</label>
-                <input type="text"
-                    id="search_name"
-                    name="search_name"
-                    value={ name }
-                    className="form-control"
-                    onChange={ event => setName(event.target.value) } />
+    return (
+        <>
+            <div className="d-flex flex-row justify-content-center">
+                <div className="d-flex flex-row w-50 align-items-center panel p-3 me-3">
+                    <div className="form-group flex-grow-1 mb-2">
+                        <label className="pb-3" htmlFor="search_name">Search Product</label>
+                        <div className="d-flex flex-row">
+                            <input type="text"
+                                   id="search_name"
+                                   name="search_name"
+                                   value={ name }
+                                   className="form-control flex-grow-1 me-3"
+                                   onChange={ event => setName(event.target.value) } />
+                            <button type="button" className="btn app-btn float-right" onClick={ () => props.onSearch({ name }) } >Search</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="panel p-3">
+                        <label className="ms-3 mb-2">Search Options</label>
+                        {filter}
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div className="col-2">
-            <button type="button" className="btn btn-primary btn-success float-right mt-4" onClick={ () => props.onSearch({ name }) } >Search</button>
-        </div>
-        
-        <div><br/></div> 
-        
-        {filter}
-
-    </div>
+        </>
+    );
 };
