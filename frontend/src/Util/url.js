63efd3c652180bed48148f0d26db44e8e1c9d499
Seth Potter <sapotter@smu.edu>
2021-11-30 19:43:45 -0600

    Fix url

--- frontend/src/Util/url.js
+++ frontend/src/Util/url.js
@@ -1,6 +1,6 @@
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-const ec2_url = '' // Needs url for webserver TODO
+const ec2_url = 'ec2-18-220-1-169.us-east-2.compute.amazonaws.com' // Needs url for webserver TODO
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-const ec2 = false;
+const ec2 = true;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
 export const url = ec2 ? ec2_url : 'localhost';
